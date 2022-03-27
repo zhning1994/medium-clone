fe7f02dfc071206813be4696010f854fa8a460a7
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-03-27 23:39:45 +0800

    completed left part of features

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,21 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+      <div>
+        <div className="space-y-5 px-10">
+          <h1 className="max-w-xl font-serif text-6xl">
+            <span className="underline decoration-[#fcbc2d] decoration-4">
+              Medium
+            </span>{' '}
+            is the place to let people discover,thinking and connect.
+          </h1>
+          <h2>
+            Post everything you learn and here with everyone at here to make the
+            world better.
+          </h2>
+        </div>
+        <img src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" />
+      </div>
     </div>
   )
 }
